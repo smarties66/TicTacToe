6384423355e99a1c1dcc091be1ac20b7c3458605
Sverrir <sverrir1993@gmail.com>
2015-10-18 11:50:22 +0000

    added the TicTacToeLogic class along with it's test class and build scripts

---
+++ src/main/java/is/ru/tictactoe/TicTacToeLogic.java
@@ -0,0 +1,7 @@
+package is.ru.tictactoe;
+
+public class TicTacToeLogic
+{
+	public static void main(String args[]) {
+	}
+}
---
+++ src/test/java/is/ru/tictactoe/TicTacToeLogicTest.java
@@ -0,0 +1,11 @@
+package is.ru.tictactoe;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TicTacToeLogicTest
+{
+	public static void main(String args[]) {
+		org.junit.runner.JUnitCore.main("is.ru.tictactoe.TicTacToeLogicTest");
+	}
+}
