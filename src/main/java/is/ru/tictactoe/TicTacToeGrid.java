96057dfa9d35be79b256c9a1005b11c5702d2a8d
Sverrir <sverrir1993@gmail.com>
2015-10-28 18:12:47 +0000

    testGetGrid passed

---
+++ src/main/java/is/ru/tictactoe/TicTacToeGrid.java
@@ -0,0 +1,29 @@
+package is.ru.tictactoe;
+
+public class TicTacToeGrid {
+
+	/****************************
+	[0][1][2]
+	[3][4][5]
+	[6][7][8]
+	****************************/
+	private Character[] grid;
+	private Character winnerToken = null;
+	private int[][] possibleWins = 
+	{
+		{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+		{0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+		{0, 4, 8}, {2, 4, 6}
+	};
+
+	public TicTacToeGrid() {
+		grid = new Character[9];
+	}
+
+	public Character[] getGrid(){
+		return grid;
+	}
+
+	public void insertTokenToSlot(char token, int slotIndex) {
+	}
+}
---
+++ src/test/java/is/ru/tictactoe/TicTacToeGridTest.java
@@ -0,0 +1,24 @@
+package is.ru.tictactoe;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TicTacToeGridTest {
+
+	public static void main(String args[]) {
+		org.junit.runner.JUnitCore.main("is.ru.tictactoe.TicTacToeGridTest");
+	}
+
+	@Test
+	public void testGetGrid() {
+		Character[] expectedGrid = 
+		{ 
+			null, null, null, 
+			null, null, null, 
+			null, null, null,
+		};
+
+		TicTacToeGrid grid = new TicTacToeGrid(); 
+		assertArrayEquals(expectedGrid, grid.getGrid());
+	}
+}
